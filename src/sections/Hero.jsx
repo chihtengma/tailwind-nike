a9e3575989e7e4a7f75235e905275a14b169ffb4
ChihTeng Ma <chihtengma416@gmail.com>
2023-09-11 23:20:22 -0400

    done'

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -10,9 +10,9 @@ const Hero = () => {
 
    return (
       <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
-         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-            <p className="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
-            <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+         <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
+            <p className="text-xl font-montserrat text-coral-red">Our Summer collections</p>
+            <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
                <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
                <br />
                <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
@@ -23,8 +23,8 @@ const Hero = () => {
             <Button label="Shop now" iconUrl={arrowRight} />
 
             <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-               {statistics.map((stat) => (
-                  <div key={stat.label}>
+               {statistics.map((stat, idx) => (
+                  <div key={idx}>
                      <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
                      <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
                   </div>
@@ -42,8 +42,8 @@ const Hero = () => {
             />
 
             <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-               {shoes.map((shoe) => (
-                  <div key={shoe}>
+               {shoes.map((shoe, idx) => (
+                  <div key={idx}>
                      <ShoeCard
                         imgUrl={shoe}
                         changeBigShoeImage={(shoe) => {
